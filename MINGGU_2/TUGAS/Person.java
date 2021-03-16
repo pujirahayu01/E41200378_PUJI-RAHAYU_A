e5ecf33503cdfc3fb402b6b95926c02fca305f63
pujirahayu01 <puji89503@gmail.com>
2021-03-16 15:22:47 +0700

    Create Person.java

---
+++ MINGGU_2/TUGAS/Person.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_mg_2;
+
+/**
+ *
+ * @author User
+ */
+public class Person {
+     public static void main(String[] args) {
+        // deklarasi variabel
+        String fName, IName, stuStatus;
+        int stuId;
+
+        // inisialisasi variabel / pemberian nilai
+        fName = " Puji ";
+        IName = "Rahayu";
+        stuId = 123456789;
+        stuStatus = "Active";
+
+        System.out.println("Student Name :" + fName + IName);
+        System.out.println("Student ID :" + stuId);
+        System.out.println("Student Status :" + stuStatus);
+
+    }
+}
