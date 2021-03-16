6dd682ebe19c718a6a5f63480e20bf8ad218e26a
pujirahayu01 <puji89503@gmail.com>
2021-03-16 15:22:52 +0700

    Create TUGAS_C.java

---
+++ MINGGU_2/TUGAS/TUGAS_C.java
@@ -0,0 +1,24 @@
+
+package tugas;
+
+
+public class TUGAS_C {
+    public static void main (String[] args){
+       
+        boolean gameOver = false;
+        
+        int students = 50, clases= 3;
+        
+        double sales_tax;
+        
+        short number1;
+        
+        int beOrNot2be;
+        
+        float priceindex;
+        
+        double lastYearsPrice;
+        
+        long clas;
+    }
+}
