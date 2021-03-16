8e7bbb453cba59a941f047671d011903ff3eea33
pujirahayu01 <puji89503@gmail.com>
2021-03-16 15:22:23 +0700

    Create latihan2.java

---
+++ MINGGU_2/LATIHAN/latihan2.java
@@ -0,0 +1,17 @@
+
+package latihan;
+
+public class latihan2 {
+
+    public static void main(String[] args) {
+        String nama = "indonesia";
+        char abjad = 'a';
+        int a = 7;
+        double b = 6.9;
+
+        System.out.println(nama);
+        System.out.println(a);
+
+    }
+
+}
