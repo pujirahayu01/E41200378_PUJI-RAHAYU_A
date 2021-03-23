1e277e7eab0ad39c6f8e492e413de7c87b16a7b7
pujirahayu01 <puji89503@gmail.com>
2021-03-23 11:41:31 +0700

    Create latihan_1C.java

---
+++ MINGGU_3/LATIHAN/latihan_1C.java
@@ -0,0 +1,24 @@
+
+package latihanmg_2;
+
+
+public class latihan_1C {
+    public static void main (String [] args){
+     // array 2 dimensi
+    int[][] angka = {{5,4,2,1,5}, {9,6,5,7,8}, {5,2,1,5,10}};
+    
+    for (int baris = 0; baris<3; baris++){
+        for(int kolom = 0; kolom<5; kolom ++){
+            System.out.print(angka[baris][kolom] + "  ");
+       
+        }
+        System.out.println("");
+}
+    
+    
+    
+    
+    
+    }
+
+}
