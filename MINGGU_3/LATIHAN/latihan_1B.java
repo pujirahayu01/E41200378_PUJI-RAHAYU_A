6020f78255845fd237b9d68e350746313cb7798d
pujirahayu01 <puji89503@gmail.com>
2021-03-23 11:41:26 +0700

    Create latihan_1B.java

---
+++ MINGGU_3/LATIHAN/latihan_1B.java
@@ -0,0 +1,22 @@
+package latihanmg_2;
+
+public class latihan_1B {
+
+    public static void main(String[] args) {
+        // array satu dimensi
+
+        int[] angka = {5, 10, 17, 20, 1};
+        String[] teks = {"saya", "sedang", "belajar", "OOP", "Java"};
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+
+        for (int j = 0; j < teks.length; j++) {
+            System.out.print(teks[j]);
+            System.out.print(" ");
+
+        }
+    }
+
+}
