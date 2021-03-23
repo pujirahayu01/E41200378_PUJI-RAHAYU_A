068f71c033a209800d1337b83cadf0dae045bcb3
pujirahayu01 <puji89503@gmail.com>
2021-03-23 11:41:51 +0700

    Create Tugas_1B.java

---
+++ MINGGU_3/TUGAS/Tugas_1B.java
@@ -0,0 +1,13 @@
+
+package TUGAS_MG3;
+
+public class Tugas_1B {
+
+    public static void main(String[] args) {
+       int[] array = {2,5,3,35,15,20};
+       
+       System.out.println(array.length);
+       
+
+    }
+}
