f70d5f5197e0408367f7b93a4eb0da35d4bdbde5
pujirahayu01 <puji89503@gmail.com>
2021-03-23 11:41:21 +0700

    Create latihan_1A.java

---
+++ MINGGU_3/LATIHAN/latihan_1A.java
@@ -0,0 +1,11 @@
+package latihanmg_2;
+
+public class latihan_1A {
+
+    public static void main(String[] args) {
+        int[] angka = {3, 7, 8, 1, 10, 20, 35};
+
+        System.out.println("Jumlah elemen/ indeks angka array adalah = " + angka.length);
+    }
+
+}
