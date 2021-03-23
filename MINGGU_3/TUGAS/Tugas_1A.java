dd3b89b04f703ea99d0e4748baaf64a508c92abb
pujirahayu01 <puji89503@gmail.com>
2021-03-23 11:41:46 +0700

    Create Tugas_1A.java

---
+++ MINGGU_3/TUGAS/Tugas_1A.java
@@ -0,0 +1,15 @@
+
+package TUGAS_MG3;
+
+public class Tugas_1A {
+
+    public static void main(String[] args) {
+        // membuat array 1 dimensi dengan type data string
+
+        String[] nama = {" Puji", "Rahayu"};
+        for (int i = 0; i < nama.length; i++) {
+            System.out.print(nama[i]);
+            System.out.print("  ");
+        }
+    }
+}
