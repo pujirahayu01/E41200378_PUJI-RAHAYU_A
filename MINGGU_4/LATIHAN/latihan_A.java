6064eac58ab35de455bbc68df32e857d1877168c
pujirahayu01 <puji89503@gmail.com>
2021-03-30 09:59:06 +0700

    Create latihan_A.java

---
+++ MINGGU_4/LATIHAN/latihan_A.java
@@ -0,0 +1,17 @@
+
+package minggu_4;
+
+public class latihan_A {
+    public static void main(String[] args){
+        int i = 0;
+        
+        while (i<=10){
+            // blok kode yang akan di ulang
+            System.out.println("Perulangan ke - " + i);
+        i++;
+        
+        }
+        
+    }
+    
+}
