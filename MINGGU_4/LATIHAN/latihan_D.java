b69bcd13503b7b71fc2c971b4e739b8461d6aac6
pujirahayu01 <puji89503@gmail.com>
2021-03-30 09:59:32 +0700

    Create latihan_D.java

---
+++ MINGGU_4/LATIHAN/latihan_D.java
@@ -0,0 +1,14 @@
+
+package minggu_4;
+
+
+public class latihan_D {
+    public static void main(String[] args){
+        int angka[] = {3,1,42,24,12};
+        
+        // menggunakan perulangan for each untuk menmpilkan angka
+        for (int x: angka){
+            System.out.print(x+ " ");
+        }
+    }
+}
