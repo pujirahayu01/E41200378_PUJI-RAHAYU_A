640e6d9abda51503266af3dc4532cea1465d0e4b
pujirahayu01 <puji89503@gmail.com>
2021-03-30 09:59:14 +0700

    Create latihan_B.java

---
+++ MINGGU_4/LATIHAN/latihan_B.java
@@ -0,0 +1,16 @@
+
+package minggu_4;
+
+public class latihan_B {
+    public static void main(String[] args){
+        // statement do while
+        // membuat variabel
+        
+        int i = 0; 
+        do {
+        System.out.println ("perulangan ke -" + i);
+        i++;       
+        } while (i <= 10);
+    
+    }
+}
