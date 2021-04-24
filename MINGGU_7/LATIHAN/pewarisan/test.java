b17b4dc22384c5bf9baa34ac0a8398e7237a389a
pujirahayu01 <puji89503@gmail.com>
2021-04-24 22:00:55 +0700

    Create test.java

---
+++ MINGGU_7/LATIHAN/pewarisan/test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pewarisan;
+
+public class test {
+    public static void main(String[] args){
+    pelajar mahasiswa = new pelajar("PUJI", 150 , 43 ,
+                    "E41200378" , " POLIJE" ,100);
+    System.out.println(mahasiswa.toString());
+    }
+    
+}
