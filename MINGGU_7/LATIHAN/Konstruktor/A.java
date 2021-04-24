ca46d4aea908fe484c101cc9322a80a8bdbfcbd9
pujirahayu01 <puji89503@gmail.com>
2021-04-24 21:59:47 +0700

    Create A.java

---
+++ MINGGU_7/LATIHAN/Konstruktor/A.java
@@ -0,0 +1,12 @@
+
+package Konstruktor;
+
+public class A {
+    A(){}
+        A(String param1){
+        
+//        System.out.println("Konstruktor  class A dieksekusi....");
+             System.out.println("Konstruktor  class A dengan parameter " + param1);
+        }
+        
+}
