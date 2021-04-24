1baf038fc9ffd485095e1a5e94ac4b3b9b2be583
pujirahayu01 <puji89503@gmail.com>
2021-04-24 21:59:55 +0700

    Create B.java

---
+++ MINGGU_7/LATIHAN/Konstruktor/B.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Konstruktor;
+
+/**
+ *
+ * @author User
+ */
+public class B extends A {
+    B(){
+        super("test");
+    System.out.println("Konstruktor class B di eksekusi ....");
+    }
+    
+}
