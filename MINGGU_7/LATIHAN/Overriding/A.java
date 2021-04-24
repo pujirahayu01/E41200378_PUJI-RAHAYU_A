c29ac511da86166fec7b62862f8605f8003618de
pujirahayu01 <puji89503@gmail.com>
2021-04-24 22:00:10 +0700

    Create A.java

---
+++ MINGGU_7/LATIHAN/Overriding/A.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Overriding;
+
+/**
+ *
+ * @author User
+ */
+public class A {
+    public void tampilKelayar(){
+    System.out.println("Method milik class A dipanggil");
+    }
+    
+}
