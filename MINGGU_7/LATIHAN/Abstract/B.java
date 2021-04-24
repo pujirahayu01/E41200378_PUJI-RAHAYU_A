4b6fb5fb10381ba13826c98d4a1bec8a5c1a72b2
pujirahayu01 <puji89503@gmail.com>
2021-04-24 21:59:37 +0700

    Create B.java

---
+++ MINGGU_7/LATIHAN/Abstract/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abstract;
+
+/**
+ *
+ * @author User
+ */
+public class B extends A {
+    public void  method2(){ // method abstrak di override dan dibuat
+        
+     System.out.println(" method abstract yang sudah menjadi konkrit dalam class B");
+    } 
+}
